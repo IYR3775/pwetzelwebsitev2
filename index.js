9843a87094f2a43073d150040d9268a903388ea1
IYA4546 <66664082+IYR3775@users.noreply.github.com>
2022-05-05 19:38:12 +0000

    Changes from repl

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const settings = require("./settings.json");
-const commands =  require("./commands.json");
+const commands = require("./commands.json");
 const path = require("path");
 const express = require("express");
 const app = express();
+const Topgg = require('@top-gg/sdk')
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 const errorHandler = require('express-error-handler'),
-    handler = errorHandler({
-        static: {
-            '404': path.join(__dirname, './views/404.html')
-        }
-});
+  handler = errorHandler({
+    static: {
+      '404': path.join(__dirname, './views/404.html')
+    }
+  });
 
-app.get("/", (req, res) => {
-    res.render('index', { bot: settings.website })
+const api = new Topgg.Api(process.env.top);
+
+app.get("/", async (req, res) => {
+  let stats = await api.getStats('723112579584491571').catch(() => { });
+  if (!stats) {
+    stats = { serverCount: '3700+' }
+  }
+  res.render('index', { bot: settings.website, stats: stats })
 })
 
 app.get("/commands", (req, res) => {
@@ -29,6 +36,10 @@ app.get("/thankyou", (req, res) => {
   res.render("thankyou", { bot: settings.website })
 })
 
+app.get("/credits", (req, res) => {
+  res.render("credits", { bot: settings.website })
+})
+
 app.get("/legal", (req, res) => {
   res.render("legal", { bot: settings.website })
 })
@@ -37,6 +48,6 @@ app.use(errorHandler.httpError(404));
 
 app.use(handler);
 
-app.listen(4000, function () {
-    console.log('Listening on port 4000');
+app.listen(4000, function() {
+  console.log('Listening on port 4000');
 })
